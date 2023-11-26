99cfe24384282f8077bc481c0eb6263311ec233d
k4itrun <truchorko2222@gmail.com>
2023-11-26 00:25:09 -0500

    Z

--- src/pages/api/repos.js
+++ src/pages/api/repos.js
@@ -3,7 +3,7 @@ import k4itrunConfig from "../../../k4itrun.config";
 export default async (req, res) => {
     let _ = await (await fetch('https://api.github.com/users/' + k4itrunConfig.githubName + '/repos', {
         headers: {
-            Authorization: 'token '+ "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"
+            Authorization: 'token '+ process.env.GITHUB_KEY
         }
     })).json();
 
