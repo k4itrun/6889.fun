11f412a25ce6ff856c4b31d733c0052f1c00290d
k4itrun <103044629+k4itrun@users.noreply.github.com>
2024-03-09 04:31:17 -0500

    Update k4itrun.config.js

--- k4itrun.config.js
+++ k4itrun.config.js
@@ -3,7 +3,7 @@ module.exports = {
     version: process.env.VERSION_SITE || '1.0',
     githubName: process.env.GITHUB_NAME || 'k4itrun',
     discordId: process.env.DISCORD_ID || '1187553265177608273',
-    webhook: process.env.WEBHOOK || 'https://discord.com/api/webhooks/1193233847895212032/6i7RYIz7lgt_an6FYoPOZwwM0Z7QBHSdixhTT_b-8SHsrwgoEMy3eBTK4oqTfupjzqni',
+    webhook: process.env.WEBHOOK || '',
     mongoDB: process.env.MONGO_DB || '',
     githubKey: process.env.GITHUB_KEY || '',
     discordToken: process.env.DISCORD_TOKEN || '',
