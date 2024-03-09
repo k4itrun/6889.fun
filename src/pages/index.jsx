2b664bb7aadcb48f602e28c6d539059f4aeb3292
k4itrun <103044629+k4itrun@users.noreply.github.com>
2024-03-09 04:32:47 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head' 
+import Image from 'next/image' 
 import Link from "next/link";
 import { Transition, Dialog } from '@headlessui/react'
 import { Fragment } from 'react'
