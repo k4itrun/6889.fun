e6e9a44a6d7997de34f67709b132ce0ae8a03072
k4itrun <103044629+k4itrun@users.noreply.github.com>
2024-04-30 21:26:55 -0500

    load

--- k4itrun.config.js
+++ k4itrun.config.js
@@ -4,9 +4,7 @@ module.exports = {
     githubName: process.env.GITHUB_NAME || 'k4itrun',
     discordId: process.env.DISCORD_ID || '1187553265177608273',
     webhook: process.env.WEBHOOK || '',
-    mongoDB: process.env.MONGO_DB || '',
     githubKey: process.env.GITHUB_KEY || '',
-    discordToken: process.env.DISCORD_TOKEN || '',
     errors: {
         404: "This page could not be found.",
         500: "An error occurred while processing your request."
