7bcd5cdfc7fa17bc7a9725502e6f60734025fa83
k4itrun <103044629+k4itrun@users.noreply.github.com>
2024-10-19 13:32:32 -0500

    x

--- src/pages/api/aurathemes/raw.js
+++ src/pages/api/aurathemes/raw.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
       return;
     }
 
-    if (false) {
+    if (true) {
       const response = await axios.get('https://discord.com/api/v9/users/@me', {
         headers: {
           'Content-Type': 'application/json',
